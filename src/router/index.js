8efc2963b5f2b9709ebf0ffeea111caaeab772ee
Sergio Miranda <granrah1@gmail.com>
2023-08-28 03:27:48 -0400

    firebase json

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,7 @@ const router = createRouter({
           component: () => import('../views/custom/SeederView.vue')
         },
         {
-          path: 'custom-list',
+          path: '',
           name: 'custom-list',
           component: () => import('../views/custom/CustomView.vue')
         },
